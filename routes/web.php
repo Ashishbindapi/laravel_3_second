8eb08f559b02c6ae262fc35d86b4b596bc07e49c
ashishbindraji <167048373+ashishbindraji@users.noreply.github.com>
2024-06-25 09:16:26 +0530

    Set up a fresh Laravel app

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\AdminContrller;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('user/')->group(function(){
+    Route::controller(UserController::class)->group(function(){
+        Route::get('create', 'create');
+        Route::get('list', 'list');
+        Route::get('update', 'update');
+        Route::get('delete', 'delete');
+        Route::get('{id}', 'show');
+    });
+});
+
+Route::resource('admin', AdminContrller::class);
